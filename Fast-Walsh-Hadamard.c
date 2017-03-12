b8021f7a53857dc41bacf892ef46386712e377c5
Luis Villaseñor <lvillasen@gmail.com>
2017-03-11 23:46:50 -0600

    Update Fast-Walsh-Hadamard.c

--- Fast-Walsh-Hadamard.c
+++ Fast-Walsh-Hadamard.c
@@ -46,10 +46,10 @@ int main (int argc,char *argv[]) {
     for (int i = 0; i < N; ++i) {A[i] = B[i];}
   }
   for (int i = 0 ; i < 10; i++) {
-    printf("i= %d B[i] %.5f:\n",i,B[i]);
+    printf("B[%d] = %.5f:\n",i,B[i]);
   }
   for (int i = N -10 ; i < N; i++) {
-    printf("i= %d  B[i] %.5f:\n",i,B[i]);
+    printf("B[%d] = %.5f:\n",i,B[i]);
   }
   return 0;
 }
